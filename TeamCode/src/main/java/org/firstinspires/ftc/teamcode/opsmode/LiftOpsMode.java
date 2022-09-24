d8f8b6e3884d89d7c6db8b6e970ac26f18b7b442
sammypbaird <sammypbaird@gmail.com>
2022-09-23 21:09:55 -0600

    Adding a sample opcode to have the motor go forward and backward repeatedly

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opsmode/LiftOpsMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opsmode/LiftOpsMode.java
@@ -2,41 +2,55 @@ package org.firstinspires.ftc.teamcode.opsmode;
 
 import static org.firstinspires.ftc.robotcore.external.BlocksOpModeCompanion.telemetry;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
 
-public class LiftOpsMode extends OpMode {
 
+@TeleOp(name="Basic: Linear OpMode", group="Linear Opmode")
+public class LiftOpsMode extends LinearOpMode {
+
+    // Declare OpMode members.
     private ElapsedTime runtime = new ElapsedTime();
+    private DcMotor liftDrive;
 
     @Override
-    public void init() {
+    public void runOpMode() {
         telemetry.addData("Status", "Initialized");
-    }
+        telemetry.update();
 
-    /*
-     * Code to run when the op mode is first enabled goes here
-     * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#start()
-     */
-    @Override
-    public void init_loop() {
-    }
+        liftDrive = hardwareMap.get(DcMotor.class, "lift_drive");
 
-    /*
-     * This method will be called ONCE when start is pressed
-     * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#loop()
-     */
-    @Override
-    public void start() {
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
         runtime.reset();
-    }
 
-    /*
-     * This method will be called repeatedly in a loop
-     * @see com.qualcomm.robotcore.eventloop.opmode.OpMode#loop()
-     */
-    @Override
-    public void loop() {
-        telemetry.addData("Status", "Run Time: " + runtime.toString());
+        double power = 0.1;
+        boolean isDirectionForward = true;
+        long begin = System.currentTimeMillis();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+            //flip the direction after 1 second
+            if (System.currentTimeMillis() - begin > 1000)
+            {
+                begin = System.currentTimeMillis();
+                isDirectionForward = !isDirectionForward;
+                power = -power;
+            }
+
+            // Send calculated power to wheels
+            liftDrive.setPower(power);
+
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Motor", "lift (%.2f)", liftDrive);
+            telemetry.update();
+        }
     }
 }
