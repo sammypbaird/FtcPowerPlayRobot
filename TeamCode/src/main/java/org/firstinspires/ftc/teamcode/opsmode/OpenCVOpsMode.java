e2b481a008022433be5bb88f6f98e28319154f28
sammypbaird <sammypbaird@gmail.com>
2022-10-08 21:53:51 -0600

    Getting OpenCV Sample working!

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opsmode/OpenCVOpsMode.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode.opsmode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+import org.openftc.easyopencv.OpenCvInternalCamera;
+
+@TeleOp(name="OpenCV")
+public class OpenCVOpsMode extends LinearOpMode implements OpenCvCamera.AsyncCameraOpenListener {
+
+    private OpenCvCamera camera;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+
+        WebcamName webcamName = hardwareMap.get(WebcamName.class, "webcam");
+
+        // With live preview
+        camera = OpenCvCameraFactory.getInstance().createWebcam(webcamName, cameraMonitorViewId);
+        camera.openCameraDeviceAsync(this);
+        waitForStart();
+
+    }
+
+    @Override
+    public void onOpened()
+    {
+        telemetry.addData("Status", "Webcam initialized");
+        telemetry.update();
+        camera.startStreaming(320, 240, OpenCvCameraRotation.UPRIGHT);
+    }
+
+    @Override
+    public void onError(int errorCode)
+    {
+        telemetry.addData("Status", "Error initializing camera");
+        telemetry.update();
+    }
+}
