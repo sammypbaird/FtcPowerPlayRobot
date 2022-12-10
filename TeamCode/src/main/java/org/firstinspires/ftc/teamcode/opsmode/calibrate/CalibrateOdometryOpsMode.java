c49b3bcf37dda4e2a87bad739375266218817823
sammypbaird <sammypbaird@gmail.com>
2022-12-10 11:40:51 -0700

    Adding empty class for calibrating odometry

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opsmode/calibrate/CalibrateOdometryOpsMode.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.opsmode.calibrate;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@TeleOp(name="Calibrate Odometry")
+public class CalibrateOdometryOpsMode extends LinearOpMode {
+
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //initialization
+
+
+        waitForStart();
+
+        while (opModeIsActive()) {
+
+            
+            telemetry.update();
+
+        }
+    }
+}
